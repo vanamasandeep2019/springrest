f172a433d7a3eb26396464de5b7cf8d1159717b9
krishna sandeep <vanamasandeep2019@gmail.com>
2020-02-27 11:06:11 +0530

    Path Variable and returning Bean

--- src/main/java/com/example/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/example/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,18 @@ public class HelloWorldController {
 	@GetMapping(path="/hellojava")
 	public String helloJava(){
 		return "Hello Java";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean getHelloWordBean(){
+		return new HelloWorldBean("Hello World Bean");
+		
+	}
+	
+	@GetMapping(path="/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean getHelloWordPathVariable(@PathVariable String name){
+		return new HelloWorldBean(name);
+		
 	}
 
 }
